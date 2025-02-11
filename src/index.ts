7b6e1bb328dee9f71f4e29ba6c1923c24f1612db
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-02-11 22:58:29 +0530

    learn use od middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, urlencoded } from 'express';
 
 const app=express();
-const PORT=3000;
+const PORT=3001;
+
+
+app.use(urlencoded())                  //This is a middleware or we can assume that this is a type of plugin
 
 app.get('/', (req:Request, res:Response) => {
     res.send('Hello World!')
   })
+
+app.post('/users',(req:Request,res:Response)=>{
+    console.log(req.body)
+    res.send(`this is your response from post req ${JSON.stringify(req.body)}`)
+})
 
 
 app.listen(PORT,()=>console.log(`Server is started at port ${PORT}`))
