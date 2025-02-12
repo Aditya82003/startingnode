7f2291ba0075e0dd56adf6ab8d78fb8a01c06b32
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-02-12 23:11:46 +0530

    Learning more about middleware

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,22 @@ const PORT=3001;
 
 app.use(urlencoded())                  //This is a middleware or we can assume that this is a type of plugin
 
+app.use((res,req,next)=>{
+  console.log("hello from custom middleware")
+  console.log(res.body)
+  next()
+})
+
 app.get('/', (req:Request, res:Response) => {
     res.send('Hello World!')
   })
+
+app.get('/user',(req:Request,res:Response)=>{
+  console.log(req.query)
+  res.send(`This is your user route ${JSON.stringify(req.query)}`)
+})  
+
+
 
 app.post('/users',(req:Request,res:Response)=>{
     console.log(req.body)
