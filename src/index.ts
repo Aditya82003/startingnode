01fa8c1fc9857f0556df1069550b4c04f03bd804
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-02-15 23:27:01 +0530

    Added api for accessing mock data

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,42 @@
 import express, { Request, Response, urlencoded } from 'express';
-
-const app=express();
-const PORT=3001;
+import users from '../MOCK_DATA.json'
 
 
+const app = express();
+const PORT = 3001;
+
+app.use(express.json())                 // Parse request body and put json in your request body
 app.use(urlencoded())                  //This is a middleware or we can assume that this is a type of plugin
 
-app.use((res,req,next)=>{
+app.use((res, req, next) => {
   console.log("hello from custom middleware")
-  console.log(res.body)
   next()
 })
 
-app.get('/', (req:Request, res:Response) => {
-    res.send('Hello World!')
-  })
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-app.get('/user',(req:Request,res:Response)=>{
-  console.log(req.query)
-  res.send(`This is your user route ${JSON.stringify(req.query)}`)
-})  
+app.get('/users', (req: Request, res: Response) => {
+  const html = `
+  <ul>
+  ${users.map((user) => `<li>${user.first_name}</li>`)}
+  </ul>
+  `
+  res.send(html)
+})
 
-
-
-app.post('/users',(req:Request,res:Response)=>{
-    console.log(req.body)
-    res.send(`this is your response from post req ${JSON.stringify(req.body)}`)
+app.get('/users/api',(req,res)=>{
+  res.send(users)
 })
 
 
-app.listen(PORT,()=>console.log(`Server is started at port ${PORT}`))
+
+
+app.post('/users', (req: Request, res: Response) => {
+  console.log(req.body)
+  res.send(`this is your response from post req ${JSON.stringify(req.body)}`)
+})
+
+
+app.listen(PORT, () => console.log(`Server is started at port ${PORT}`))
